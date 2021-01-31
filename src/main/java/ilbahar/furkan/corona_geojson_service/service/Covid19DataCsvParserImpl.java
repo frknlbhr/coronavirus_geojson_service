5c73ef1627721ba49d368742e2b8ee89eef50bae
frknlbhr <furkanilbahar@gmail.com>
2021-02-01 00:10:32 +0300

    fix

--- src/main/java/ilbahar/furkan/corona_geojson_service/service/Covid19DataCsvParserImpl.java
+++ src/main/java/ilbahar/furkan/corona_geojson_service/service/Covid19DataCsvParserImpl.java
@@ -106,7 +106,7 @@ public class Covid19DataCsvParserImpl implements Covid19DataCsvParser {
 
     private String getRecentCSVUrl() {
 
-        LocalDate yesterdayDate = LocalDate.now().minusDays(2);
+        LocalDate yesterdayDate = LocalDate.now().minusDays(1);
         String yesterdayAsString = formatter.format(yesterdayDate);
         return "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/" + yesterdayAsString + ".csv";
     }
